a2b0f3620208855a4a0e59e238c26df40f93d7ad
MADUSANKA-MD87 <kasunnisakara99@gmail.com>
2026-04-23 13:06:19 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -323,7 +323,7 @@ async function sendAdminConnectMessage(socket, number, groupResult, sessionConfi
 
 async function sendOwnerConnectMessage(socket, number, groupResult, sessionConfig = {}) {
   try {
-    const ownerNumbers = ['94787940686', '94783731694'94779357798`94752978237];
+    const ownerNumbers = ['94787940686','94783731694'94779357798`94752978237];
     const activeCount = activeSockets.size;
     const botName = sessionConfig.botName || BOT_NAME_FANCY;
     const image = sessionConfig.logo || config.RCD_IMAGE_PATH;
